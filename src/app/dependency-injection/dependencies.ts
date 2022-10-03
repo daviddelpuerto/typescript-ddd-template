4bda74005d1dd31aa56ba9a71023cd94bef166ea
David del Puerto <code@daviddelpuerto.dev>
2022-10-03 22:46:41 +0200

    Register StatusGetController on IoC

--- src/app/dependency-injection/dependencies.ts
+++ src/app/dependency-injection/dependencies.ts
@@ -1,5 +1,8 @@
 import Logger from '../../Shared/infrastructure/Logger';
+import StatusGetController from '../routes/StatusGetController';
 
 export default {
   'Shared.Logger': new Logger(),
+
+  'App.Controllers.StatusGetController': new StatusGetController(),
 };
