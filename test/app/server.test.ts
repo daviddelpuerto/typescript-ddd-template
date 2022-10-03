f99ab3902d96f05b96eab69e550a798e15d6275d
David del Puerto <code@daviddelpuerto.dev>
2022-10-03 22:58:15 +0200

    Created test script and first test to test app health check GET route

---
+++ test/app/server.test.ts
@@ -0,0 +1,23 @@
+import '../../src/Shared/infrastructure/bootstrap';
+import { Server } from '../../src/app/Server';
+import request from 'supertest';
+import Logger from '../../src/Shared/infrastructure/Logger';
+
+const logger = new Logger('Test-App');
+
+const port = process.env.NODE_PORT || '3000';
+const server = new Server(port, logger);
+
+beforeAll(async () => {
+  await server.listen();
+});
+
+afterAll(async () => {
+  await server.stop();
+});
+
+test('Sending a GET request to the root endpoint returns status code 200', async () => {
+  await request(server.httpServer)
+    .get('/api')
+    .expect(200);
+});
