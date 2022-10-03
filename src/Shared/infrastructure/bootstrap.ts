102f686c78d41bdc248bc1e68f7324b3f8cb5ba8
David del Puerto <code@daviddelpuerto.dev>
2022-10-03 22:14:08 +0200

    Created bootstrap module and added it to app entry point

---
+++ src/Shared/infrastructure/bootstrap.ts
@@ -0,0 +1,9 @@
+import Logger from './Logger';
+
+const logger = new Logger('Bootstrap');
+
+(function boostrap() {
+  logger.info('🚧 Starting boostrap process');
+
+  logger.info('🏁 Finished bootsrap process\n');
+})();
