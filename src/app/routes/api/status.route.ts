1d3e111e2cff3c57cea8888f84ea187a00d8f69b
David del Puerto <code@daviddelpuerto.dev>
2022-10-07 15:30:46 +0200

    Created function to register api routes

---
+++ src/app/routes/api/status.route.ts
@@ -0,0 +1,8 @@
+import { Express } from 'express';
+import { container } from '../../dependency-injection';
+import GetStatusController from './GetStatusController';
+
+export function register(app: Express) {
+  const getStatusController: GetStatusController = container.get('App.Controllers.GetStatusController');
+  app.get('/api', getStatusController.run.bind(getStatusController));
+}
