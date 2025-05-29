939f9c62e0e945cf02b13b49bc0a78c178c2fd1c
David del Puerto <code@daviddelpuerto.dev>
2025-05-29 15:43:16 +0200

    Merge main and index files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-import './main';
+import bootstrap from './Shared/infrastructure/bootstrap';
+import App from './app/App';
+
+(async () => {
+  await bootstrap();
+  const app = new App(process.env.NODE_PORT || '3000');
+  await app.start();
+})();
