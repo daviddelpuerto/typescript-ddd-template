f8e3b90a43d8a2139bd2294f067d65618aa05a75
David del Puerto <code@daviddelpuerto.dev>
2025-05-29 16:04:06 +0200

    Uninstall dotenv package and instead use --env-file to load .env files

--- src/Shared/infrastructure/bootstrap.ts
+++ src/Shared/infrastructure/bootstrap.ts
@@ -1,5 +1,4 @@
 import Logger from './Logger';
-import loadEnvConfig from '../../../config';
 import injectDependencies from '../../app/dependency-injection';
 
 export default async function boostrap() {
@@ -7,7 +6,6 @@ export default async function boostrap() {
 
   logger.info('🚧 Starting boostrap process');
 
-  loadEnvConfig();
   await injectDependencies();
 
   logger.info('🏁 Finished bootsrap process');
